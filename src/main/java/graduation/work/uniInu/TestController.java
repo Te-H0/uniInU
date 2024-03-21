d82513ec142e6dc398202d998a5795134d36fb79
teho <xogh8755@naver.com>
2024-03-21 19:25:55 +0900

    ci/cd test api 생성

--- src/main/java/graduation/work/uniInu/TestController.java
+++ src/main/java/graduation/work/uniInu/TestController.java
@@ -3,6 +3,7 @@ package graduation.work.uniInu;
 import graduation.work.uniInu.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private final UserService userService;
 
+    @GetMapping("/test")
+    public String testApi() {
+        return "yes~";
+    }
 
 }
 
